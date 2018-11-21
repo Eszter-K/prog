666c81b0bee4e7176c4ddb276191772d9d83dd26
Eszter K <eszter.k.93@gmail.com>
2018-11-21 20:27:09 +0100

    Question 2

--- assignment-1.R
+++ assignment-1.R
@@ -1,11 +1,16 @@
 
-# Read forbes.csv ---------------------------------------------------------
+# Question 1 --------------------------------------------------------------
 
 library(tidyverse)
-
 forbes <- read_csv("forbes.csv")
 forbes$rank <- parse_number(forbes$rank)
-forbes$net_worth <- parse_number(forbes$net_worth)
 forbes$age <- parse_integer(forbes$age, na = "-")
+forbes$net_worth[1572:1578] <- "0.9" #values recorded in millions are set to billions, 
+#to avoid confusion after parsing
+forbes$net_worth <- parse_number(forbes$net_worth)
 type_convert(forbes)
+
+# Question 2 --------------------------------------------------------------
+forbes <- filter(forbes, net_worth >= 1)
+
 
